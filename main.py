90f396fb743f861fd41a762cbfe0e2ce4ea58d83
Felipe Braz <felipebraz.fb333@gmail.com>
2024-08-08 18:34:03 -0300

    Adicionei arquivo main

---
+++ main.py
@@ -0,0 +1,22 @@
+from vision import *
+def main():
+    while True:
+
+        #captura de imagens
+        cod = valida_item()
+        if cod == 1:
+            print("O objeto depositado é do tipo metal\n")
+            #acionar servo motor 1
+
+        elif cod == 2:
+            print("O objeto depositado é do tipo plástico\n")
+            # acionar servo motor 2
+
+        #inicio do jogo
+
+        print("Muito bem! o jogo será iniciado em instantes...")
+        break
+
+
+if __name__ == "__main__":
+    main()
--- vision.py
+++ vision.py
@@ -2,50 +2,62 @@ from keras.models import load_model  # TensorFlow is required for Keras to work
 import cv2  # Install opencv-python
 import numpy as np
 
-# Disable scientific notation for clarity
-np.set_printoptions(suppress=True)
 
-# Load the model
-model = load_model("keras_model.h5", compile=False)
+def valida_item():
 
-# Load the labels
-class_names = open("labels.txt", "r").readlines()
+    # Disable scientific notation for clarity
+    np.set_printoptions(suppress=True)
 
-# CAMERA can be 0 or 1 based on default camera of your computer
-camera = cv2.VideoCapture(0)
+    # Load the model
+    model = load_model("/home/keep/PycharmProjects/Projeto_Eco/keras_model.h5", compile=False)
 
-while True:
-    # Grab the webcamera's image.
-    ret, image = camera.read()
+    # Load the labels
+    class_names = open("/home/keep/PycharmProjects/Projeto_Eco/labels.txt", "r").readlines()
 
-    # Resize the raw image into (224-height,224-width) pixels
-    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+    # CAMERA can be 0 or 1 based on default camera of your computer
+    camera = cv2.VideoCapture(0)
 
-    # Show the image in a window
-    cv2.imshow("Webcam Image", image)
+    while True:
+        # Grab the webcamera's image.
+        ret, image = camera.read()
 
-    # Make the image a numpy array and reshape it to the models input shape.
-    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+        # Resize the raw image into (224-height,224-width) pixels
+        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
-    # Normalize the image array
-    image = (image / 127.5) - 1
+        # Show the image in a window
+        cv2.imshow("Webcam Image", image)
 
-    # Predicts the model
-    prediction = model.predict(image)
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
+        # Make the image a numpy array and reshape it to the models input shape.
+        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
 
-    # Print prediction and confidence score
-    print("Class:", class_name[2:], end="")
-    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+        # Normalize the image array
+        image = (image / 127.5) - 1
 
-    # Listen to the keyboard for presses.
-    keyboard_input = cv2.waitKey(1)
+        # Predicts the model
+        prediction = model.predict(image)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
 
-    # 27 is the ASCII for the esc key on your keyboard.
-    if keyboard_input == 27:
-        break
+        # Print prediction and confidence score
+        print("Class:", class_name[2:], end="")
+        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
 
-camera.release()
-cv2.destroyAllWindows()
+        # Listen to the keyboard for presses.
+        keyboard_input = cv2.waitKey(1)
+
+        print(confidence_score)
+        print(class_name[2:])
+
+        if (class_name[2:] == "metal\n") and (confidence_score > 0.95):
+            return 1
+
+        if (class_name[2:] == "plastico\n") and (confidence_score > 0.95):
+            return 2
+
+        # 27 is the ASCII for the esc key on your keyboard.
+        if keyboard_input == 27:
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
